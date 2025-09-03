c199043480100a20a2123e9b3e9204b418ba0241
UltraSive <github@aidanperry.net>
2025-09-03 03:26:40 +0000

    added a systemd service file and renamed the binary

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,8 +1,6 @@
 package server
 
 import (
-	"encoding/json"
-	"log"
 	"net/http"
 
 	"libvirt-controller/internal/server/handlers"
@@ -78,16 +76,4 @@ func (s *Server) RegisterRoutes() http.Handler {
 	})
 
 	return r
-}
-
-func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	resp := make(map[string]string)
-	resp["message"] = "Hello World"
-
-	jsonResp, err := json.Marshal(resp)
-	if err != nil {
-		log.Fatalf("error handling JSON marshal. Err: %v", err)
-	}
-
-	_, _ = w.Write(jsonResp)
 }
